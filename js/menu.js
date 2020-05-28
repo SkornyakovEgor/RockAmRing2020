6afbfa5c0ce584acffc9273fb07f6a9a8bc63219
Ottrer19 <skornyakovegor231199@gmail.com>
2020-05-29 02:36:07 +0500

    v 2.2.1

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+$(function(){
+
+    $('.menuToggle').on('click', function() {
+       $('.main_menu').slideToggle(300, function(){
+            if( $(this).css('display') === "none"){
+                $(this).removeAttr('style');
+            }
+       });
+
+    });
+
+});
