acf7b2d7afbca8c91955402d9b1dd8aab5b88feb
Ottrer19 <skornyakovegor231199@gmail.com>
2020-06-02 19:34:41 +0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -19,7 +19,7 @@ $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, 
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
 $mail->setFrom('andycruze@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('skornyakovegor231199@gmail.com');     // Кому будет уходить письмо 
+$mail->addAddress('egor1199@mail.ru');     // Кому будет уходить письмо 
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
